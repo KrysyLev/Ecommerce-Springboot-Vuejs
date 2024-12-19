10e73ee0f60719014c4f798955dd3a44178319a0
Krysy Lev <119929320+KrysyLev@users.noreply.github.com>
2024-12-19 18:19:58 +0700

    update

---
+++ tecmi-ecommerce-backend-master/src/main/java/com/sena/tecmiecommercebackend/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.sena.tecmiecommercebackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8081") // Specify the frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*") // Allow all headers
+                        .allowCredentials(true); // Allow credentials (cookies)
+            }
+        };
+    }
+}
